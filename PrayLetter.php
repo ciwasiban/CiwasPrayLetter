7050432d69c8827cf5c813c341af8de23d16771d
ciwasiban <ciwasiban@gmail.com>
2019-12-05 17:26:44 +0800

    ciwas's style.

--- PrayLetter.php
+++ PrayLetter.php
@@ -2,35 +2,36 @@
 class PrayLetter
 {
     /** 設定寄件者資訊 **/
-    public $mailer = "LDT 領袖門訓宣教計畫";
-    public $username = "xxx@gmail.com";
+    public $mailer = "LDT 領袖門訓宣教計畫同工 曉芬";
+    public $username = "xx@gmail.com";
     public $password = "password";
 
     /** 設定信件內容 **/
-    public $subject = '平安，這是我們門訓宣教計劃七月的代禱信';
+    public $subject = '平安，這是我們門訓宣教計劃11月的代禱信';
     public $body = '';
 
     // 招呼語
-    public $hola = '感謝神帶領我們在七月的門訓計劃上有美好的突破，';
+    public $hola = '感謝神帶領我們在11月的門訓計劃上有美好的突破，';
 
     // 本月焦點
-    public $letterUrl = 'http://ldtmission.blogspot.tw/2019/07/letter.html';
-    public $focusTitle1 = '看見建立門訓牧養團隊的需要';
-    public $focusTitle2 = '神興起領袖門徒成為我的幫助';
-    public $focusTitle3 = '讓領袖門徒逐步接手門訓小組';
-    public $focusTitle4 = '【門徒見證】不再是憑自己做乃是靠著主來做';
+    public $letterUrl = 'http://bit.ly/2sI9Z59';
+    public $letterUrl1 = 'http://bit.ly/2LoyZER';
+    public $letterUrl2 = 'http://bit.ly/2Pg2rOI';
+    public $letterUrl3 = 'http://bit.ly/2LoJVT4';
+    public $focusTitle1 = '見證神使軟弱的我變為剛強';
+    public $focusTitle2 = '看見我們是屬靈生命的園丁';
+    public $focusTitle3 = '伸出鐮刀收割屬神成熟莊稼';
 
     public $focusPoint1 = Array();  // setting in function setFocusPoint()
     public $focusPoint2 = Array();
     public $focusPoint3 = Array();
-    public $focusPoint4 = Array();
 
     // 奉獻情形
-    public $donationOfThisMonth = '39,110';    // 本月收到奉獻
-    public $expenditureOfThisMonth = '63,526'; // 每月支出
-    public $yearlyDonation = '857,169';        // 年度總共需要奉獻
-    public $donationOfThisYear = '591,040';    // 實際收到奉獻
-    public $annualShortfall = '266,129';       // 年度尚缺奉獻金額
+    public $donationOfThisMonth = '25,000';    // 本月收到奉獻
+    public $expenditureOfThisMonth = '24,414'; // 每月支出
+    public $yearlyDonation = '307,044';        // 年度總共需要奉獻
+    public $donationOfThisYear = '206,306';    // 實際收到奉獻
+    public $annualShortfall = '100,738';       // 年度尚缺奉獻金額
 
     function __construct() {
         // init
@@ -45,7 +46,6 @@ class PrayLetter
             '{%focuspoint1.alt%}', '{%focuspoint1.url%}',
             '{%focuspoint2.alt%}', '{%focuspoint2.url%}',
             '{%focuspoint3.alt%}', '{%focuspoint3.url%}',
-            '{%focuspoint4.alt%}', '{%focuspoint4.url%}',
             '{%letterUrl%}',
             '{%donationOfThisMonth%}', '{%expenditureOfThisMonth%}', '{%yearlyDonation%}', '{%donationOfThisYear%}', '{%annualShortfall%}'
             );
@@ -53,7 +53,6 @@ class PrayLetter
             $this->focusPoint1['alt'], $this->focusPoint1['url'],
             $this->focusPoint2['alt'], $this->focusPoint2['url'],
             $this->focusPoint3['alt'], $this->focusPoint3['url'],
-            $this->focusPoint4['alt'], $this->focusPoint4['url'],
             $this->letterUrl,
             $this->donationOfThisMonth, $this->expenditureOfThisMonth, $this->yearlyDonation, $this->donationOfThisYear, $this->annualShortfall
         );
@@ -93,9 +92,8 @@ class PrayLetter
     }
 
     function setFocusPoint() {
-        $this->focusPoint1 = Array('alt'=> $this->focusTitle1, 'url'=> $this->letterUrl . '#focus_point1');
-        $this->focusPoint2 = Array('alt'=> $this->focusTitle2, 'url'=> $this->letterUrl . '#focus_point2');
-        $this->focusPoint3 = Array('alt'=> $this->focusTitle3, 'url'=> $this->letterUrl . '#focus_point3');
-        $this->focusPoint4 = Array('alt'=> $this->focusTitle4, 'url'=> $this->letterUrl . '#focus_point4');
+        $this->focusPoint1 = Array('alt'=> $this->focusTitle1, 'url'=> $this->letterUrl1);
+        $this->focusPoint2 = Array('alt'=> $this->focusTitle2, 'url'=> $this->letterUrl2);
+        $this->focusPoint3 = Array('alt'=> $this->focusTitle3, 'url'=> $this->letterUrl3);
     }
 }
